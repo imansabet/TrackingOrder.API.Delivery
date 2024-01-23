406a121d97c5721a1e3bcabdac7a56c5a91963d0
iman <dev.imansabet@gmail.com>
2024-01-23 21:47:41 +0330

    Add Entity (OrderTracking)

---
+++ TrackingOrder.API.Delivery/INfrastructure/Entities/OrderTracking.cs
@@ -0,0 +1,14 @@
+﻿namespace TrackingOrder.API.Delivery.INfrastructure.Entities
+{
+    public class OrderTracking
+    {
+        public Guid OrderId { get; set; }
+        public string Status { get; set; }
+        public DateTime UpdateDate { get; set; }    
+        public string Location { get; set; }    
+        public string PhoneNumber { get; set; }    
+        public string DeliveryPerson { get; set; } 
+        public string DeliveryVehicle { get; set; } 
+        public bool IsDelivered { get; set; } 
+    }
+}
