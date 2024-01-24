d948d2effbeb0d05190f2e71a3bd6e151439b71d
iman <dev.imansabet@gmail.com>
2024-01-24 19:24:55 +0330

    Add Interface repository : IOrderTrackingRepository

---
+++ TrackingOrder.API.Delivery/INfrastructure/Repository/IOrderTrackingRepository.cs
@@ -0,0 +1,14 @@
+﻿using TrackingOrder.API.Delivery.DTO.InternalAPIModel.Request;
+using TrackingOrder.API.Delivery.DTO.InternalAPIModel.Response;
+
+namespace TrackingOrder.API.Delivery.INfrastructure.Repository
+{
+    public interface IOrderTrackingRepository
+    {
+        Task<List<TrackingOrderResponse>> GetAllOrderTrackingsAsync();
+        Task<TrackingOrderResponse> GetOrderTrackingByIdAsync(OrderIdRequest orderIdRequest);
+        Task AddOrderTrackingAsync(TrackingOrderResponse orderTrackingResponse);
+        Task UpdateOrderTrackingAsync(TrackingOrderResponse orderTrackingResponse);
+        Task DeleteOrderTrackingAsync(OrderIdRequest orderIdRequest);
+    }
+}
---
+++ TrackingOrder.API.Delivery/Migrations/20240124154933_AddDTOInternalModels.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrackingOrder.API.Delivery.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDTOInternalModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "OrderTrackings",
+                keyColumn: "OrderId",
+                keyValue: new Guid("ab3c1a63-fefa-4934-824b-e4b5201cb84a"),
+                column: "UpdateDate",
+                value: new DateTime(2024, 1, 24, 19, 19, 32, 877, DateTimeKind.Local).AddTicks(7807));
+
+            migrationBuilder.UpdateData(
+                table: "OrderTrackings",
+                keyColumn: "OrderId",
+                keyValue: new Guid("f7948d66-ae41-4a2b-8a41-a5dea5750243"),
+                column: "UpdateDate",
+                value: new DateTime(2024, 1, 24, 19, 19, 32, 877, DateTimeKind.Local).AddTicks(7788));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "OrderTrackings",
+                keyColumn: "OrderId",
+                keyValue: new Guid("ab3c1a63-fefa-4934-824b-e4b5201cb84a"),
+                column: "UpdateDate",
+                value: new DateTime(2024, 1, 23, 22, 1, 35, 146, DateTimeKind.Local).AddTicks(3390));
+
+            migrationBuilder.UpdateData(
+                table: "OrderTrackings",
+                keyColumn: "OrderId",
+                keyValue: new Guid("f7948d66-ae41-4a2b-8a41-a5dea5750243"),
+                column: "UpdateDate",
+                value: new DateTime(2024, 1, 23, 22, 1, 35, 146, DateTimeKind.Local).AddTicks(3376));
+        }
+    }
+}
